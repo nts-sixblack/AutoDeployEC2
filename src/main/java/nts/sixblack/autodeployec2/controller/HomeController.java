2e1da0cffb0a0a16b0468f01ee47286ac4fb7c06
nts-sixblack <nts.sixblack@gmail.com>
2022-07-22 09:19:03 +0700

    docker-compose.yml

--- src/main/java/nts/sixblack/autodeployec2/controller/HomeController.java
+++ src/main/java/nts/sixblack/autodeployec2/controller/HomeController.java
@@ -1,7 +1,6 @@
 package nts.sixblack.autodeployec2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +17,5 @@ public class HomeController {
     }
 
     @GetMapping("/abc")
-    public String abc(){return "abc";}
+    public String abc(){return "abcd";}
 }
